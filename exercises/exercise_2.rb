47a13543f3b88b20709f582a0f0c57432b0145b5
Jesse McDermott <mcdermott.jesse9@gmail.com>
2021-03-30 00:46:04 +0000

    remove comments and change name of first store

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,8 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+puts @store2.name
+@store1.name = "Dodd's Store" 
+puts @store1.name
