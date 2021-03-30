256749055d5dd8a3bbaa219fdd144e17f52c464d
Jesse McDermott <mcdermott.jesse9@gmail.com>
2021-03-30 01:15:54 +0000

    remove unused variable from ex1 and load subset of stores based on specific criteria

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -6,4 +6,17 @@ require_relative './exercise_3'
 puts "Exercise 4"
 puts "----------"
 
-# Your code goes here ...
+Store.create(name: "Surrey",annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true)
+@mens_stores.each do |stores|
+puts "#{stores.name}\n#{stores.annual_revenue}"
+end
+@womens_stores = Store.where(womens_apparel: true)
+@womens_stores.each do |stores|
+  if stores.annual_revenue < 1000000
+  puts "#{stores.name}\n#{stores.annual_revenue}"
+end
+end
