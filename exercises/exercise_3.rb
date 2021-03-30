68f4f99204ab01b7842fb79f0b5dad99b97646e2
Jesse McDermott <mcdermott.jesse9@gmail.com>
2021-03-30 00:51:08 +0000

    delete the third store using Active Record's destroy method

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -5,4 +5,8 @@ require_relative './exercise_2'
 puts "Exercise 3"
 puts "----------"
 
-# Your code goes here ...
+@store3 = Store.find_by(id: 3)
+puts @store3.name
+@store3.destroy
+
+puts Store.count
