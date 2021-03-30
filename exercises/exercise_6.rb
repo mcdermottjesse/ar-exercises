b56395781ecd56770b9726d9d7f384ae653b091c
Jesse McDermott <mcdermott.jesse9@gmail.com>
2021-03-30 03:31:16 +0000

    relate employees table to store id from store table and add data

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,22 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+class Store < ActiveRecord::Base
+has_many :employees
+end
+
+class Employee < ActiveRecord::Base
+  belongs_to :store
+end
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Tommy", last_name: "Lee", hourly_rate: 80)
+@store1.employees.create(first_name: "Pam", last_name: "Anderson", hourly_rate: 85)
+
+@store2.employees.create(first_name: "Sebastion", last_name: "Bach", hourly_rate: 55)
+@store2.employees.create(first_name: "Tiggy", last_name: "McD", hourly_rate: 60)
+@store2.employees.create(first_name: "James", last_name: "Brown", hourly_rate: 70)
+
+employee_table = Employee.all
+
+p employee_table
